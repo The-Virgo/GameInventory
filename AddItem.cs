3b1994d6fcee52881ddb0c34d73d79b4a1850657
ETHANCPIPKIN <lucidfridaynights@gmail.com>
2021-08-11 23:13:06 -0700

    Added the ability to add items to store

--- AddItem.cs
+++ AddItem.cs
@@ -12,9 +12,59 @@ namespace GameInventory
 {
     public partial class AddItem : Form
     {
+
+
         public AddItem()
         {
             InitializeComponent();
+        }
+
+        private void addItemConfirmBtn_Click(object sender, EventArgs e)
+        {
+            int itemId = -1;
+            String itemName = itemNameTxtBx.Text;
+            String desc = descTxtBx.Text;
+            String effect = effectTxtBx.Text;
+
+
+            List<Item> items = GameDb.GetAllItems();
+
+            if (items == null)
+            {
+                itemId = 0;
+            }
+            else
+            {
+                for (int i = 0; i < items.Count; i++)
+                {
+                    if (itemName == items[i].ItemName)
+                    {
+                        itemNameTxtBx.Text = "";
+                        return;
+                    }
+                }
+
+                itemId = items.Count + 1; 
+            }
+            if (!int.TryParse(ItemValueTxtBx.Text, out int value))
+            {
+                valueTxtBx.Text = "";
+                return;
+            }
+
+            Item item = new Item();
+            item.ItemId = itemId;
+            item.ItemName = itemName;
+            item.Description = desc;
+            item.Effect = effect;
+            item.Value = value;
+
+            GameDb.Add(item); 
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
